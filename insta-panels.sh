6375601b80b7108ca927bbc291222631d35c9449
swansinflight <nigel@swansinflight.co.nz>
2020-04-05 11:40:47 +1200

    initial commit. not usable yet

---
+++ insta-panels.sh
@@ -0,0 +1,43 @@
+#!/bin/bash
+clear
+echo =====================================================
+echo This script makes square tiles from across your image
+echo =====================================================
+echo
+echo         Your tiles can be found in subfolder 
+echo                /insta-panels when done
+echo
+read -p 'How many tiles across: ' tilenumvar
+read -p 'Each tile is many pixels wide (and high): ' tilepixelvar
+echo
+echo 1 - slice the entire image. Note the bottom tiles may not be squares
+echo 2 - tiles from across the top
+echo 3 - tiles from across the centre
+echo 4 - tiles from across the bottom
+read -p 'Enter the number of where you want your tiles from: ' gravitynumvar
+echo 
+echo -----------------------------------------------------
+echo NOTE: script errors out when it runs out of images.
+echo  Dont worry!
+echo -----------------------------------------------------
+echo
+
+let resizevar=$tilenumvar*$tilepixelvar
+
+for i in ./*.jpg ./*.jpeg ./*.png
+do
+    if [ ! -d "./insta-panels" ]; then mkdir "./insta-panels"; fi
+    filename=$(basename "$i")
+    /bin/convert \
+    -filter Lanczos \
+    "$i" \
+    -set option:filter:filter Lanczos \
+    -set option:filter:blur 0.8 \
+    -resize $[resizevar]x$[resizevar] \
+    -quality 95 \
+    -crop $[tilepixelvar]x$[tilepixelvar] +repage +adjoin \
+    "./insta-panels/$filename"
+done
+echo 
+echo ===================== FINISHED ======================
+echo 
