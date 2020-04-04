4bed96d68ee616891213c58739e5e0174e8c9cbc
swansinflight <nigel@swansinflight.co.nz>
2020-04-04 15:12:33 +1300

    added linux scripts

---
+++ insta3tile.sh
@@ -0,0 +1,15 @@
+#!/bin/bash
+for i in ./*.jpg ./*.jpeg ./*.png
+do
+    if [ ! -d "./insta" ]; then mkdir "./insta"; fi
+    filename=$(basename "$i")
+    /bin/convert \
+    -filter Lanczos \
+    "$i" \
+    -set option:filter:filter Lanczos \
+    -set option:filter:blur 0.8 \
+    -resize 3240x3240 \
+    -quality 95 \
+    -crop 1080x1080 +repage +adjoin \
+    "./insta/$filename"
+done
